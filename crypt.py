15423ec0f343718f8a354110b2a44b5ca0da8d17
kralsam <kralsam@gmail.com>
2018-11-14 11:04:32 +0300

    Türkçe karakter sorunu vardı artık yok.

--- crypt.py
+++ crypt.py
@@ -1,31 +1,41 @@
 #
-#cryptorn
+#cryptorn - Tamamen hobi amaçlı eğlencesine denenmiştir.
 #
 import hashlib
 
-myhash = hashlib.sha256('1234'.encode("utf-8")).hexdigest()
+password = "Bismillah :)"
+myhash = hashlib.sha256(password.encode("utf-8")).hexdigest()
 
 def changeChar(achar,key):
     ch = ord(achar)^ord(key)
     return chr(ch)
 
-def cryptStr(msg,hash):
+#Test-OK!
+#print(changeChar("Ş","c"))
+#print(changeChar(changeChar("Ş","c"),"c"))
+
+
+def cryptStr(_msg, _hash):
     keyArray = []
     cryptMsgTmp = []
     cryptMsg = ''
 
-    for ch in myhash:
-        keyArray.append(ch.encode("utf-8"))
+    # for ch in myhash:
+    #     keyArray.append(chr(ch.encode("utf-8")))
     
+    #Test-OK!
+    # print(changeChar("Ş", _hash[3]))
+    # print(changeChar(changeChar("Ş", _hash[3]), _hash[3]))
+    # print(_hash[3])
+
     i=0
-    for cha in msg:
-        cryptMsgTmp.append(changeChar(cha.encode("utf-8"),keyArray[i%512]));i = i+1 
-    
+    for cha in _msg:
+        cryptMsgTmp.append(changeChar(cha,_hash[(i*i)%len(_hash)]));i = i+1 
     cryptMsg = ''.join(cryptMsgTmp)
 
     return cryptMsg
 #print(changeChar(45,34))
 #print(changeChar(changeChar(45,34),34))
 
-print(cryptStr("Selamunaleykum",myhash))
-print(cryptStr(cryptStr("Selamunaleykum",myhash),myhash))
+print(cryptStr("Selamünaleyküm hacı osman nabin nası gidii?(Giresun Ağzı)",myhash))
+print(cryptStr(cryptStr("Selamünaleyküm hacı osman nabin nası gidii?(Giresun Ağzı)",myhash),myhash))
